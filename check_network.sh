0686bb3317b3201981780b38bfbce47605db1d0f
Daniel <daniel_13@outlook.com>
2023-03-17 12:13:26 +0100

    Update script bash network

--- check_network.sh
+++ check_network.sh
@@ -1,1 +1,16 @@
+#!/bin/bash
 
+# Get public IP address
+public_ip=$(curl -s http://ifconfig.me)
+
+# Resolve hostname
+hostname=$(hostname)
+
+# Check connectivity with router
+ping -c 1 8.8.8.8 
+if [ $? -eq 0 ]; then
+  echo "Public IP address: $public_ip"
+  echo "Hostname: $hostname"
+else
+  echo "No connectivity with internet"
+fi
